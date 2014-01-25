236c9d7b3ab31859a64b12d2328a4df415772e89
Michael Kompanets <mkompanets@gmail.com>
2014-01-25 13:41:23 -0800

    Create default_gemfile.rb
    Gems that always have to be initially added.

---
+++ default_gemfile.rb
@@ -0,0 +1,30 @@
+# Default Development and Test Gemfile.
+# rails new blog -m ~/template.rb
+# rails new blog -m http://example.com/template.rb
+
+# rake rails:template LOCATION=~/template.rb
+# rake rails:template LOCATION=http://example.com/template.rb
+
+gem_group :development, :test do
+  gem 'rspec-rails'
+  gem 'guard-livereload'
+  gem 'spring'
+end
+
+gem_group :test do
+  gem 'rspec-rails'
+  gem 'factory_girl_rails'
+  gem 'faker'
+  gem 'capybara'
+  gem 'guard-rspec'
+  gem 'guard-spring'
+  gem 'shoulda-matchers'
+end
+
+run('bundle install')
+generate('rspec:install')
+run('bundle exec spring binstub --all')
+run('bundle binstubs rspec-core')
+run('guard init livereload')
+run('guard init rspec')
+run('guard init spring')
